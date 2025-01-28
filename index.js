56cc8e042669caf80c0bceacf52a4efd6a5d50a6
isylllane <lutik301@gmail.com>
2025-01-28 15:36:07 +0300

    init

--- index.js
+++ index.js
@@ -16,58 +16,54 @@ bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
-  if (text === "/start") {
-    await bot.sendMessage(
-      chatId,
-      "Заходи в наш интернет магазин по кнопке ниже",
-      {
-        reply_markup: {
-          inline_keyboard: [
-            [{ text: "Сделать заказ", web_app: { url: webAppUrl } }],
-          ],
-        },
-      }
-    );
-    await bot.sendMessage(chatId, "Ниже появится кнопка, заполни форму", {
+  if(text === '/start') {
+    await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
       reply_markup: {
         keyboard: [
-          [{ text: "Заполнить форму", web_app: { url: webAppUrl + "/form" } }],
-        ],
-      },
-    });
+          [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+        ]
+      }
+    })
+
+    await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+      reply_markup: {
+        inline_keyboard: [
+          [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+        ]
+      }
+    })
   }
-  if (msg?.web_app_data?.data) {
+  if(msg?.web_app_data?.data) {
     try {
-      const data = JSON.parse(msg?.web_app_data?.data);
-      await bot.sendMessage(chatId, "Спасибо за обратную связь!");
-      await bot.sendMessage(
-        chatId,
-        "Данные: " + data.country + " " + data.street
-      );
+      const data = JSON.parse(msg?.web_app_data?.data)
+      console.log(data)
+      await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+      await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
+      await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
+
+      setTimeout(async () => {
+        await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+      }, 3000)
     } catch (e) {
       console.log(e);
     }
   }
 });
 
-app.post("/web-data", async (req, res) => {
-  const {queryId, products, totalPrice} = res.body;
+app.post('/web-data', async (req, res) => {
+  const {queryId, products = [], totalPrice} = req.body;
   try {
     await bot.answerWebAppQuery(queryId, {
       type: 'article',
       id: queryId,
       title: 'Успешная покупка',
-      input_message_content: {massage_text: 'Поздравляем с покупкой, вы приобрели това на сумму ' + totalPrice},
-    });
-    return res.status(200).send({});
-  }catch(e)  {
-    await bot.answerWebAppQuery(queryId, {
-      type: 'article',
-      id: queryId,
-      title: 'Не удалось приобрести товар',
-      input_message_content: {massage_text: 'Не удалось приобрести товар'},
-    });
-    return res.status(500).end();
+      input_message_content: {
+        message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+      }
+    })
+    return res.status(200).json({});
+  } catch (e) {
+    return res.status(500).json({})
   }
 })
 
